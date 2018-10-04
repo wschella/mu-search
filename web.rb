22b852c4dba5ec834985daf541721515ac0809b5
type <nathaniel.rudavsky@gmail.com>
2018-10-03 18:37:20 -0700

    Implement barebones elasticsearch api

--- web.rb
+++ web.rb
@@ -1,10 +1,85 @@
+require 'net/http'
+
+class Elastic
+  def initialize(host: 'localhost', port: 9200)
+    @host = host
+    @port = port
+    @port_s = port.to_s
+  end
+
+  def run(uri, req)
+    req['content-type'] = 'application/json'
+
+    res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+      http.request(req)
+    end
+
+    case res
+    when Net::HTTPSuccess, Net::HTTPRedirection
+      res.body
+    else
+      res.value
+    end
+  end
+
+  def create_index(index, mappings = nil)
+    uri = URI("http://#{@host}:#{@port_s}/#{index}")
+    req = Net::HTTP::Put.new(uri)
+    if mappings
+      req.body = { mappings: { _doc: { properties: mappings } } }.to_json
+    end
+
+    run(uri, req)
+  end
+
+  def get_document(index, id)
+    uri = URI("http://#{@host}:#{@port_s}/#{index}/_doc/#{id}")
+    req = Net::HTTP::Get.new(uri)
+    run(uri, req)
+  end
+
+  def put_document(index, id, document)
+    uri = URI("http://#{@host}:#{@port_s}/#{index}/_doc/#{id}")
+    req = Net::HTTP::Put.new(uri)
+    req.body = document
+    run(uri, req)
+  end
+
+  def update_document(index, id, document)
+    uri = URI("http://#{@host}:#{@port_s}/#{index}/_doc/#{id}/_update")
+    req = Net::HTTP::Post.new(uri)
+    req.body = document
+    run(uri, req)
+  end
+
+  def delete_document(index, id)
+    uri = URI("http://#{@host}:#{@port_s}/#{index}/_doc/#{id}")
+    req = Net::HTTP::Delete.new(uri)
+    run(uri, req)
+  end
+
+  def search(index:, query_string: nil, query: nil, sort: nil)
+    if query_string
+      uri = URI("http://#{@host}:#{@port_s}/#{index}/_search?q=#{query_string}&sort=#{sort}")
+      req = Net::HTTP::Post.new(uri)
+    else 
+      uri = URI("http://#{@host}:#{@port_s}/#{index}/_search")
+      req = Net::HTTP::Post.new(uri)
+      req.body = query.to_json
+    end
+
+    run(uri, req)    
+  end
+
+end
+
 get "/test" do
   content_type 'application/json'
 
-  { data: { message: "it works!" } }.to_json
-end
+  client = Elastic.new(host: 'elasticsearch', port: 9200)
 
-get "/
-error" do
-  error "No way!"
+  # client.create_index 'user', { title: { type: "text" }, name: { type: "text" } }
+  # client.put_document('user', 1, { name: "John Doe" }).body
+  # client.get_document('people', 1).body
+  client.search index: 'peopl*,user', query: { query: { match: { name: "John" } } }
 end
