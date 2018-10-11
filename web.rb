26ba9f317dabe12ce2dcd22d39071042884ef81f
type <nathaniel.rudavsky@gmail.com>
2018-10-10 19:18:21 -0700

    ... working on prototype [overwrite] ...

--- web.rb
+++ web.rb
@@ -70,9 +70,10 @@ class Elastic
 
     run(uri, req)    
   end
-
 end
 
+
+# test
 get "/test" do
   content_type 'application/json'
 
@@ -82,4 +83,93 @@ get "/test" do
   # client.put_document('user', 1, { name: "John Doe" }).body
   # client.get_document('people', 1).body
   client.search index: 'peopl*,user', query: { query: { match: { name: "John" } } }
+end
+
+
+# index documents
+get "/index" do
+  content_type 'application/json'
+  client = Elastic.new(host: 'elasticsearch', port: 9200)
+
+  query_result = query <<SPARQL
+    SELECT ?id ?title ?desc WHERE {
+      ?doc <http://mu.semte.ch/vocabularies/core/uuid> ?id; 
+           <http://mu.semte.ch/vocabularies/core/title> ?title; 
+           <http://mu.semte.ch/vocabularies/core/description> ?desc
+    }
+SPARQL
+
+  query_result.each do |result|
+    id = result[:id].to_s
+
+    groups_query_result = query <<SPARQL
+      SELECT ?gid WHERE {
+        ?doc <http://mu.semte.ch/vocabularies/core/uuid> "#{result.id.to_s}";
+             <http://mu.semte.ch/vocabularies/authorization/inGroup> ?group.
+        ?group <http://mu.semte.ch/vocabularies/core/uuid> ?gid
+      }
+SPARQL
+    groups = []
+    groups_query_result.each do |group|
+      groups.push(group[:gid].to_s)
+    end
+
+    document = {
+      title: result[:title].to_s,
+      description: result[:description].to_s,
+      groups: groups,
+      required_matches: 1
+    }
+    log.info document
+    client.put_document('document', id, document.to_json)
+  end
+
+
+  "all ok"
+end
+
+
+
+# search
+get "/search" do
+  content_type 'application/json'
+  client = Elastic.new(host: 'elasticsearch', port: 9200)
+
+  # test query
+  query_result = query <<SPARQL
+    SELECT ?id WHERE {
+      ?doc <http://mu.semte.ch/vocabularies/core/uuid> ?id
+    } LIMIT 1
+SPARQL
+
+  search_query = 
+    {
+      query: {
+        bool: {
+          must: [
+            {
+              match: {
+                title: "document"
+              }
+            },
+            {
+              terms_set: {
+                groups: {
+                  terms: [
+                    "g02"
+                  ],
+                  minimum_should_match_field: "required_matches"
+                }
+              }
+            }
+          ]
+        }
+      }
+    } 
+
+  results = client.search index: 'document', query: search_query 
+  # log.info JSON.parse(results)
+  # JSON.parse(results)["hits"]["hits"]
+  results
+  
 end
