3b48e2d5912d7eda94dba8cd94047db77abea138
type <nathaniel.rudavsky@gmail.com>
2018-10-17 19:45:12 -0700

    Thread authorization through search

--- web.rb
+++ web.rb
@@ -36,7 +36,7 @@ class Elastic
     end
   end
 
-  def create_index(index, mappings = nil)
+  def create_index index, mappings = nil
     uri = URI("http://#{@host}:#{@port_s}/#{index}")
     req = Net::HTTP::Put.new(uri)
     if mappings
@@ -46,33 +46,39 @@ class Elastic
     run(uri, req)
   end
 
-  def get_document(index, id)
+  def refresh_index index
+    uri = URI("http://#{@host}:#{@port_s}/#{index}/_refresh")
+    req = Net::HTTP::Post.new(uri)
+    run(uri, req)
+  end
+
+  def get_document index, id
     uri = URI("http://#{@host}:#{@port_s}/#{index}/_doc/#{id}")
     req = Net::HTTP::Get.new(uri)
     run(uri, req)
   end
 
-  def put_document(index, id, document)
+  def put_document index, id, document
     uri = URI("http://#{@host}:#{@port_s}/#{index}/_doc/#{id}")
     req = Net::HTTP::Put.new(uri)
     req.body = document
     run(uri, req)
   end
 
-  def update_document(index, id, document)
+  def update_document index, id, document
     uri = URI("http://#{@host}:#{@port_s}/#{index}/_doc/#{id}/_update")
     req = Net::HTTP::Post.new(uri)
     req.body = document
     run(uri, req)
   end
 
-  def delete_document(index, id)
+  def delete_document index, id
     uri = URI("http://#{@host}:#{@port_s}/#{index}/_doc/#{id}")
     req = Net::HTTP::Delete.new(uri)
     run(uri, req)
   end
 
-  def search(index:, query_string: nil, query: nil, sort: nil)
+  def search index:, query_string: nil, query: nil, sort: nil
     if query_string
       uri = URI("http://#{@host}:#{@port_s}/#{index}/_search?q=#{query_string}&sort=#{sort}")
       req = Net::HTTP::Post.new(uri)
@@ -115,12 +121,106 @@ SPARQL
 end
 
 
+def get_all_access_rights
+  query_result = query <<SPARQL
+  SELECT ?allowedGroups ?usedGroups ?index WHERE {
+    GRAPH <http://mu.semte.ch/authorization> {
+     ?rights a <accessRights>;
+             <hasAllowedGroups> ?allowedGroups;
+             <hasUsedGroups> ?usedGroups;
+             <hasEsIndex> ?index
+    }
+  }
+SPARQL
+  
+  query_result.map do |result|
+    {
+      allowed: result["allowedGroups"].to_s.split(","), 
+      used: result["usedGroups"].to_s.split(","), 
+      index: result["index"] 
+    }
+  end
+end
+
+
+# * make real URI and mu:uuid!
+def put_access_rights index, allowed_groups, used_groups
+  uri = "<#{index}>"
+
+  query_result = query <<SPARQL
+  INSERT DATA {
+    GRAPH <http://mu.semte.ch/authorization> {
+        #{uri} a <accessRights>;
+            <hasAllowedGroups> "#{allowed_groups}";
+            <hasUsedGroups> "#{used_groups}";
+             <hasEsIndex> "#{index}"
+    }
+  }
+SPARQL
+end
+
+
+# seems overly complicated... what am I missing?
+# at this level, could probably be done directly in the triplestore
+def has_same_access_rights allowed_one, used_one, allowed_two
+  diff = allowed_one - allowed_two
+  log.info "diffs: #{diff} // #{used_one} == #{used_one - diff} ?"
+  diff == [] or (used_one - diff) == used_one
+end
+
+
+# seems overly complicated... what am I missing?
+def find_matching_rights allowed_groups
+  if allowed_groups
+    get_all_access_rights.each do |ar|
+      return ar[:index] if has_same_access_rights ar[:allowed], ar[:used], allowed_groups
+    end
+
+    nil
+  end
+end
+
+
+def current_groups
+  allowed_groups_s = request.env["HTTP_MU_AUTH_ALLOWED_GROUPS"]
+  allowed_groups = allowed_groups_s ? JSON.parse(allowed_groups_s).map { |e| e["value"] } : nil
+
+  used_groups_s = request.env["HTTP_MU_AUTH_USED_GROUPS"]
+  used_groups = used_groups_s ? JSON.parse(used_groups_s).map { |e| e["value"] } : nil
+
+  return allowed_groups.sort, used_groups.sort
+end
+
+
+def current_matching_access_rights
+  allowed_groups, used_groups = current_groups
+  find_matching_rights allowed_groups
+end
+
+
+def current_index client
+  allowed_groups, used_groups = current_groups
+  used_groups = used_groups or []
+  allowed_groups = allowed_groups or []
+  index = find_matching_rights allowed_groups
+  new = nil
+
+  unless index
+    new = true
+    index = allowed_groups.join("-")
+    put_access_rights index, allowed_groups.join(","), used_groups.join(",")
+    client.create_index index
+    make_index client, index 
+  end
+
+  return index, new
+end
+
+
 # indexes all documents (or all authorized documents,
 # if queries are routed through an authorization service)
-get "/index" do
-  content_type 'application/json'
-  client = Elastic.new(host: 'elasticsearch', port: 9200)
-
+# * should do batch updates
+def make_index client, index
   count_list = [] # for reporting
 
   settings.properties.each do |type, property_map|
@@ -145,116 +245,45 @@ SPARQL
           title: result[:title].to_s,
           description: result[:description].to_s,
         }
-        client.put_document('document', uuid, document.to_json)
+        client.put_document(index, uuid, document.to_json)
       end
 
     end
   end
 
-  { "document_types" => count_list }.to_json
+  { index: index, document_types: count_list }.to_json
 end
 
 
-def get_all_access_rights
-  query_result = query <<SPARQL
-  SELECT ?allowedGroups ?usedGroups ?index WHERE {
-    GRAPH <http://mu.semte.ch/authorization> {
-     ?rights a <accessRights>;
-             <hasAllowedGroups> ?allowedGroups;
-             <hasUsedGroups> ?usedGroups;
-             <hasEsIndex> ?index
-    }
-  }
-SPARQL
-  
-  query_result.map do |result|
-    {
-      allowed: result["allowedGroups"].to_s.split(","), 
-      used: result["usedGroups"].to_s.split(","), 
-      index: result["index"] 
-    }
-  end
-end
-
-
-def put_access_rights uri, allowed_groups, used_groups, index
-  query_result = query <<SPARQL
-  INSERT DATA {
-    GRAPH <http://mu.semte.ch/authorization> {
-        #{uri} a <accessRights>;
-            <hasAllowedGroups> "#{allowed_groups}";
-            <hasUsedGroups> "#{used_groups}";
-             <hasEsIndex> "#{index}"
-    }
-  }
-SPARQL
-end
-
-
-# seems overly complicated... what am I missing?
-# at this level, could probably be done directly in the triplestore
-def has_same_access_rights allowed_one, used_one, allowed_two
-  diff = allowed_one - allowed_two
-  log.info "diffs: #{diff} / #{used_one - used_one}"
-  diff == [] or (used_one - diff) == used_one
-end
-
-
-# seems overly complicated... what am I missing?
-def find_matching_rights allowed_groups
-  allowed_groups = allowed_groups.sort
-  get_all_access_rights.each do |ar|
-    return ar[:index] if has_same_access_rights ar[:allowed], ar[:used], allowed_groups
-  end
-
-  nil
-end
-
-
-get "/test" do
-  put_access_rights "<rights1>", "a,b,c", "a,b", "abc"
-  ar = get_all_access_rights
-  log.info ar
-  find_matching_rights(["a","b","c"]).to_json
-end
-
-
-def run_authorized_search query
-  allowed_groups_s = request.env["HTTP_MU_AUTH_ALLOWED_GROUPS"]
-  allowed_groups = allowed_groups_s ? JSON.parse(allowed_groups_s).map { |e| e["value"] } : nil
-
-  used_groups_s = request.env["HTTP_MU_AUTH_USED_GROUPS"]
-  used_groups = used_groups_s ? JSON.parse(used_groups_s).map { |e| e["value"] } : nil
-
-  search_query = 
-    {
-      query: {
-        bool: {
-          must: [
-            query,
-            { terms_set: { groups: { terms: allowed_groups, minimum_should_match_field: "required_matches" } } }
-          ]
-        }
-      }
-    } 
-
-  results = client.search index: 'document', query: search_query 
-  results
+get "/index" do
+  content_type 'application/json'
+  client = Elastic.new(host: 'elasticsearch', port: 9200)
+  make_index client, current_index(client)
 end
 
 
 post "/search" do
   content_type 'application/json'
   client = Elastic.new(host: 'elasticsearch', port: 9200)
-  run_authorized_search @json_body["query"]
+  index, new = current_index(client)
+  if new
+    make_index client, index
+    client.refresh_index index
+  end
+  client.search index: index, query: @json_body
 end
 
 
-# * title is hard-coded
 get "/search" do
   content_type 'application/json'
   client = Elastic.new(host: 'elasticsearch', port: 9200)
-  run_authorized_search( { match: { title: params["q"] } } )
+  index, new = current_index(client)
+  log.info "Using index: #{index}"
+  if new
+    make_index client, index
+    client.refresh_index index
+  end
+  client.search index: index, query: @json_body
 end
 
 
